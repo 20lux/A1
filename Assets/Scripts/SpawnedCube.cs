9ec1060c782134930f559a25ff1792ea5e32935f
Anne Truong <anne.truongxx@gmail.com>
2024-08-10 18:34:50 +1000

    Used literals as arguments to random function rather than de-referencing local object in SpawnedCube

--- Assets/Scripts/SpawnedCube.cs
+++ Assets/Scripts/SpawnedCube.cs
@@ -7,9 +7,9 @@ public class SpawnedCube : MonoBehaviour
 {
 	[SerializeField] private Rigidbody rb;
 
-	[SerializeField] private Vector2 xVelocityRange = new Vector2(-1, 1);
-	[SerializeField] private float verticalVelocity = 10;
-	[SerializeField] private Vector2 zVelocityRange = new Vector2(-1, 1);
+	//[SerializeField] private Vector2 xVelocityRange = new Vector2(-1, 1);
+	//[SerializeField] private float verticalVelocity = 10;
+	//[SerializeField] private Vector2 zVelocityRange = new Vector2(-1, 1);
 
 	[SerializeField] private int lifetime = 10000;
 
@@ -55,9 +55,9 @@ public class SpawnedCube : MonoBehaviour
 		rb = GetComponent<Rigidbody>();
 
 		rb.velocity = new Vector3(
-			Random.Range(xVelocityRange.x, xVelocityRange.y),
-			verticalVelocity, 
-			Random.Range(zVelocityRange.x, zVelocityRange.y));
+			Random.Range(-1, 1),
+			10, 
+			Random.Range(-1, 1));
 
 		rb.angularVelocity = new Vector3(
 			Random.Range(-10, 10), 
